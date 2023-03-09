45ffe82a84878a088fb60d02da1c516f50347fb7
Renato Dias <renatocardosodias@icloud.com>
2023-03-09 17:37:30 -0300

    Tutorial de auto layout com UIKit

---
+++ Lession02Project/Lession02Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lession02Project
+//
+//  Created by Renato on 09/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
